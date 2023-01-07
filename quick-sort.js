03889aab7631edb174734326aa1b1fb243758715
Berat Aras <berat.aras@hotmail.com>
2023-01-07 11:17:33 +0300

    added insertion, merge, quick sorts

---
+++ quick-sort.js
@@ -0,0 +1,37 @@
+function swap(arr, i, j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+function partition(arr, low, high) {
+
+    let pivot = arr[high];
+
+    let i = (low - 1);
+
+    for (let j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr, i, j);
+        }
+    }
+    swap(arr, i + 1, high);
+    return (i + 1);
+}
+
+function quickSort(arr, low, high) {
+    if (low < high) {
+        let pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+
+    return arr;
+}
+
+const myArray = [30, 1, 11, 22, 34, 43, -1];
+
+const toQuickSort = quickSort(myArray, 0, 6);
+
+console.log(toQuickSort);
